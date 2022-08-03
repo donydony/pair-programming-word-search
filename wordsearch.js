447ad050bce657c69c27adcceabd567ff27f636a
Donny Zhao <donny.zh@hotmail.com>
2022-08-03 14:45:06 -0400

    implemented reverse word search

--- wordsearch.js
+++ wordsearch.js
@@ -6,21 +6,48 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true;
     }
+
+    let reverseHorizontalJoin = letters;
+
+    for (let h of reverseHorizontalJoin) {
+        h = h.reverse();
+    }
+    
+    reverseHorizontalJoin = reverseHorizontalJoin.map(ls => ls.join(''));
+
+    for (hl of reverseHorizontalJoin) {
+        if (hl.includes(word)) {
+            return true;
+        }
+    }
+
     let verticalJoinArray = [];
     for (let i = 0; i < letters[0].length; i++) {
         verticalJoinArray.push([]);
-    }
-    // [[], [], []]
+    } 
 
     for (let x = 0; x < letters.length; x++) {
         for (let y = 0; y < letters[x].length; y++) {
             verticalJoinArray[y].push(letters[x][y]);
         }
     }
-
     let verticalJoin = verticalJoinArray.map(ls => ls.join(''));
+
+    let reverseVerticalJoinArray = verticalJoinArray;
+    for (let j of reverseVerticalJoinArray) {
+        j = j.reverse();
+    }
+    let reverseVerticalJoin = reverseVerticalJoinArray.map(vls => vls.join(''));
+
     for (vl of verticalJoin) {
-        if (vl.includes(word)) return true;
+        if (vl.includes(word)) {
+            return true;
+        }
+    }
+    for (vl of reverseVerticalJoin) {
+        if (vl.includes(word)) {
+            return true;
+        }
     }
     return false;
 }
@@ -35,7 +62,7 @@ const result = wordSearch([
     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-  ], 'LARRY');
+  ], 'AUQFCWA');
 
   console.log(result);
 
